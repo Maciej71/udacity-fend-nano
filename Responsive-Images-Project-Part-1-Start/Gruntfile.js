3587dbabd7e02ae33887bc52d32fee823b41ef64
Maciej Giza <maciejgiza@gmail.com>
2018-03-07 23:02:40 +0100

    finished part 1, grunt and responsive image adapted

--- Responsive-Images-Project-Part-1-Start/Gruntfile.js
+++ Responsive-Images-Project-Part-1-Start/Gruntfile.js
@@ -14,20 +14,20 @@ module.exports = function(grunt) {
         options: {
           engine: 'im',
           sizes: [{
-            /*
-            Change these:
-            
-            width: ,
-            suffix: ,
-            quality:
-            */
-          }]
+            // name: 'small',
+            width: '40%',
+            // suffix: '_small',
+            quality: 40
+          },
+          // {
+          //   name: 'large',
+          //   width: '50%',
+          //   suffix: '_large',
+          //   quality: 40
+          // }
+        ],
+        rename: false
         },
-
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
@@ -36,35 +36,8 @@ module.exports = function(grunt) {
         }]
       }
     },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
-        },
-      },
-    },
-
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
-        }]
-      },
-    },
   });
-  
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
